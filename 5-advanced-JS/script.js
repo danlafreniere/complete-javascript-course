c07036df0e9d872447a07f3c5837395e9350ce7c
Dan LaFreniere <danl@advisorwebsites.com>
2020-07-16 19:16:02 -0700

    Closures

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -85,3 +85,42 @@ birthYears = [1987, 1986, 1995, 2000, 1958];
 
 var ages = arrayCalc(birthYears, calculateAge);
 console.log(ages);
+
+
+// Immediately Invoked Function Expressions (IIFE).
+
+// Tricks the parser and makes it think we have an expression.
+// In JS, anything in parentheses cannot be a declaration, therefore, it thinks it's an expression.
+(function(goodluck) {
+  var score = Math.random() * 10;
+  // You can also pass vars in:
+  console.log(score >= 5 - goodluck);
+})(5);
+
+// score cannot be accessed from outside, creating data privacy (or code modularity).
+
+
+// Closures.
+function retirement(retirementAge) {
+  var statement = ' years left until retirement';
+  return function(yearOfBirth) {
+    var age = 2020 - yearOfBirth;
+    console.log((retirementAge - age) + statement);
+  }
+}
+
+var retirementUs = retirement(66);
+retirementUs(1987);
+// or:
+retirement(66)(1995);
+
+var retireIceland = retirement(67);
+retirementIceland(1987);
+
+
+// Think about this: we are able to still use the statement & retirementAge vars
+// even after the retirement function stopped its execution...
+// This is a closure...
+
+// Closure: an inner function always has access to the variables and
+// parameters of its outer function, even after the outer function has returned.
