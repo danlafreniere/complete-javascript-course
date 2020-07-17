652000ec7aa295fa9bedb95be2a36e4a1ed9237d
Daniel LaFreniere <daniellafreniere@MacBook-Pro.local>
2020-07-16 20:35:32 -0700

    To do list

---
+++ 6-budgety/app.js
@@ -0,0 +1,12 @@
+// Planning and Architecture
+
+/**
+ * TO DO LIST:
+ * Fundamental bits:
+ * Add event handler -> input income description/value and button to add
+ * Retrieve data from input values
+ * Add the new item to our data structure
+ * Add the new item to the UI
+ * Calculate budget
+ * Update UI.
+*/
