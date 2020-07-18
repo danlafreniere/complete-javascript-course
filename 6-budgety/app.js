b50b702dc2b88c92051a469c653a5d680dcfd21d
Daniel LaFreniere <daniellafreniere@MacBook-Pro.local>
2020-07-17 19:39:19 -0700

    Basic controller structure

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -9,4 +9,43 @@
  * Add the new item to the UI
  * Calculate budget
  * Update UI.
-*/
+ *
+ *  We will create modules to keep things nice and encapsulated.
+ * 3 key modules:
+ * - DATA MODULE - budgetController
+ * - UI MODULE - UIController
+ * - CONTROLLER MODULE - controller
+ */
+
+var budgetController = (function() {
+
+})();
+
+var UIController = (function() {
+
+})();
+
+var controller = (function(budgetCtrl, UiCtrl) {
+
+    var ctrlAddItem = function() {
+        // 1. Get the field input data.
+
+        // 2. Add the item to the budget controller.
+
+        // 3. Add the item to the UI.
+
+        // 4. Calculate the budget.
+
+        // 5. Display the budget in the UI.
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        // ENTER KEY = 13.
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+
+})(budgetController, UIController);
