5f74e710777d8561d0f74fb16141e562a26e6e56
Dan LaFreniere <danl@advisorwebsites.com>
2020-08-09 20:32:51 -0700

    Use those modules.

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,14 +1,5 @@
-import axios from 'axios';
-
 // Global app controller.
-async function getResults(query) {
-  try {
-    const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch(error) {
-    console.log(error);
-  }
-}
+import Search from './models/Search';
 
-getResults('pasta');
+const search = new Search('pizza');
+search.getResults();
