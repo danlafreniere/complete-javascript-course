0cd66fd83ba78cf3463ed75b1e32069c0f9a43f6
Dan LaFreniere <danl@advisorwebsites.com>
2020-08-09 20:23:43 -0700

    Fix polyfill and first axios call

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,1 +1,14 @@
+import axios from 'axios';
+
 // Global app controller.
+async function getResults(query) {
+  try {
+    const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+getResults('pasta');
--- 9-forkify/webpack.config.js
+++ 9-forkify/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: [
     './src/js/index.js',
-    'babel-polyfill',
+    '@babel/polyfill',
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
