4ff9b566140e4fcf150797ede2075f9bf1ed5330
danlafreniere <danl@advisorwebsites.com>
2020-08-10 14:20:07 -0700

    Recipe rendering and pagination

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,5 +1,35 @@
 // Global app controller.
 import Search from './models/Search';
+import * as searchView from './views/SearchView';
+import { elements, renderLoader, clearLoader } from './views/base';
 
-const search = new Search('pizza');
-search.getResults();
+// Global state for application.
+const state = {};
+
+elements.searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  controlSearch();
+});
+
+elements.searchResultPages.addEventListener('click', event => {
+  const button = event.target.closest('.btn-inline');
+  if (button) {
+    const goToPage = button.dataset.goto;
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, parseInt(goToPage));
+  }
+});
+
+const controlSearch = async () => {
+  // Get query from view.
+  const query = searchView.getInput();
+  if (query) {
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchResults);
+    state.search = new Search(query);
+    await state.search.getResults();
+    clearLoader();
+    searchView.renderResults(state.search.result);
+  }
+};
